20243dcd3ec59bec88dedb09826fd00e0c2bacfe
Daniel Gelling <info@danielgelling.nl>
2016-02-15 11:50:39 +0100

    Initial commit

---
+++ pluginchecker.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+Plugin Name: WP Plugin checker
+Plugin URI: http://www.deaannemers.nl
+Description: This plugin checks installed plugins for abnormalities as vulnerabilities for infection and hackability. It also checks for plugin updates. It sends out an e-mail if an abnormality or update is detected. 
+Author: Daniel Gelling
+Author e-mail: daniel@deaannemers.nl
+Author URI: http://www.deaannemers.nl
+Version: 1.0
+*/
+
+
